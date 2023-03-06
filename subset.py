2d20311679e7a273ee4d9914add6044837458bfa
David Coello Pulido <davidcoello@eprosima.com>
2023-03-06 19:58:30 +0100

    Update config and add proxy
    Signed-off-by: David Coello Pulido <davidcoello@eprosima.com>

--- subset.py
+++ subset.py
@@ -1,11 +1,13 @@
 import os
 import sys
+import time
 import json
 import argparse
 
 import sources
 import sinks
 import storages
+import proxys
 
 from core.Register import *
 from core.CLIFormat import CLIFormat
@@ -60,6 +62,12 @@ parser.add_argument(
     help="elem domain")
 
 parser.add_argument(
+    '-u', '--user',
+    type=str,
+    default="",
+    help="user")
+
+parser.add_argument(
     '-v', '--value',
     type=str,
     default="",
@@ -69,21 +77,35 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
     config = json.loads(open(args.config, "r+").read())
-    storage_t_ = REG_NAMESPACE[Type.STORAGE][config["storage"]]["instance"]
+
+    storage_t_ = REG_NAMESPACE[Type.STORAGE][config["storage"]
+                                             ["type"]]["instance"]
+    proxy_t_ = REG_NAMESPACE[Type.PROXY][config["proxy"]["type"]]["instance"]
+
     DOMAIN = config["default_domain"] if args.domain == "" else args.domain
     VALUE = REG_NAMESPACE[Type.SOURCE][config["domains"][DOMAIN]["default_source"]]["instance"].get() \
         if args.value == "" else args.value
+    USER = config["user"] if args.user == "" else args.user
+
+    proxy = proxy_t_(config["proxy"])
 
     if args.generate:
         storage_t_.create_storage(config, config["domains"])
-        sys.exit(0)
+        local = storage_t_(config["storage"])
+        for domain in config["domains"].keys():
+            proxy.update_shared_domain(USER, domain, local.get_domain(domain))
 
-    local = storage_t_(config)
+    local = storage_t_(config["storage"])
+
     if args.add:
         local.add_elem_to_domain(DOMAIN, args.index, VALUE)
+        _, elem = local.select_elem_from_domain(DOMAIN, args.index)
+        proxy.update_shared_domain_elem(USER, DOMAIN, args.index, elem)
 
     if args.remove:
         local.remove_elem_from_domain(DOMAIN, args.index)
+        _, elem = local.select_elem_from_domain(DOMAIN, args.index)
+        proxy.update_shared_domain_elem(USER, DOMAIN, args.index, elem)
 
     if args.select:
         action, elem = local.select_elem_from_domain(DOMAIN, args.index)
@@ -91,10 +113,17 @@ if __name__ == "__main__":
             REG_NAMESPACE[Type.SINK][action]["instance"].send(elem["value"])
 
     if args.list:
-        print(CLIFormat.format_domain(
-            local.get_domain(DOMAIN)))
+        if USER == config["user"]:
+            print(CLIFormat.format_domain(
+                local.get_domain(DOMAIN)))
+        else:
+            print(CLIFormat.format_domain(
+                proxy.get_shared_domain(USER, DOMAIN)))
 
     if args.reset:
         local.reset_domain(DOMAIN)
+        proxy.update_shared_domain(USER, DOMAIN, local.get_domain(DOMAIN))
 
     local.store_changes()
+    time.sleep(0.1)
+    proxy.__del__()
